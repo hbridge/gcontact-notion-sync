6db4d826dbc5bf2f5e0579b4f2fcef0c72d378e8
Henry Bridge <hbridge@oldfashionedsw.com>
2022-11-16 15:01:20 -0500

    try buildspec and appspec

---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+apt update
+pm2 update
--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /var/node/google-notion-sync
-
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash
-sudo apt-get install -y nodejs
-npm install
-# sudo npm install -g pm2
-# npm install
